e6ce30d613ed2042a786091ca7d51adf147f86c6
Hugo Marx <marx.hugo@gmail.com>
2022-03-23 00:14:14 +0100

    syntaxe clean

--- form.php
+++ form.php
@@ -7,23 +7,33 @@
     <link rel="stylesheet" href="style.css">
     <title>My Form</title>
 </head>
+
 <body>
-   <form action="thanks.php" method="POST" enctype="application/x-www-form-urlencoded">
+   
+    <form action="thanks.php" method="POST" enctype="application/x-www-form-urlencoded">
+       
        <label for="firstname">Prénom</label>
        <input type="text" name="firstname" id="firstname" placeholder="Votre nom">
+       
        <label for="lastname">Nom</label>
        <input type="text" name="lastname" id="lastname" placeholder="Votre prénom">
+       
        <label for="email">Email</label>
        <input type="email" name="email" id="email" placeholder="Votre email">
+       
        <label for="phone">Téléphone</label>
        <input type="tel" name="phone" id="phone" placeholder="Votre numéro de téléphone">
-       <select name="list" id="list">
+      
+        <select name="list" id="list">
            <option value="info"> Demande d'informations</option>
            <option value="dispo">Changement de disponibilité</option>
            <option value="reservation">Reservation de table</option>
-           <textarea name="message" id="message" cols="30" rows="10" placeholder="Votre message"></textarea>
-           <input type="submit" value="Envoyer">
-       </select>
+        </select>
+           
+        <textarea name="message" id="message" cols="30" rows="10" placeholder="Votre message"></textarea>
+           
+        <input type="submit" value="Envoyer">
+       
    </form>
 </body>
 </html>
