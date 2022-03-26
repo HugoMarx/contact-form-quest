fc15c09c536a52896eafe40a3dad8e127e6f7aac
Hugo Marx <marx.hugo@gmail.com>
2022-03-26 15:52:27 +0100

    Securisation client/serveur

--- form.php
+++ form.php
@@ -13,16 +13,15 @@
     <form action="thanks.php" method="POST" enctype="application/x-www-form-urlencoded">
        
        <label for="firstname">Prénom</label>
-       <input type="text" name="firstname" id="firstname" placeholder="Votre nom">
-       
+       <input type="text" name="firstname" id="firstname" pattern="[a-zA-Z'-'\s]*" placeholder="Votre nom" required>
        <label for="lastname">Nom</label>
-       <input type="text" name="lastname" id="lastname" placeholder="Votre prénom">
+       <input type="text" name="lastname" id="lastname" pattern="[a-zA-Z'-'\s]*"  placeholder="Votre prénom" required>
        
        <label for="email">Email</label>
-       <input type="email" name="email" id="email" placeholder="Votre email">
+       <input type="email" name="email" id="email" placeholder="Votre email" required>
        
        <label for="phone">Téléphone</label>
-       <input type="tel" name="phone" id="phone" placeholder="Votre numéro de téléphone">
+       <input type="tel" name="phone" id="phone" minlength = "10" pattern = '^[+]*([0-9]{10,13})' placeholder="Votre numéro de téléphone" required>
       
         <select name="list" id="list">
            <option value="info"> Demande d'informations</option>
@@ -30,9 +29,10 @@
            <option value="reservation">Reservation de table</option>
         </select>
            
-        <textarea name="message" id="message" cols="30" rows="10" placeholder="Votre message"></textarea>
+        <textarea name="message" id="message" cols="30" rows="10" placeholder="Votre message" required></textarea>
            
         <input type="submit" value="Envoyer">
+
        
    </form>
 </body>
--- thanks.php
+++ thanks.php
@@ -10,11 +10,28 @@
 <body> 
     <div>
         <?php 
-        
-    echo '<h1>Merci '.$_POST['firstname'].' '.$_POST['lastname'].' de nous avoir contacté à propos de “'.$_POST['list'].'”.</h1>'.
-    '<p>  Un de nos conseiller vous contactera soit à l’adresse '.$_POST['email'].' ou par téléphone au '.$_POST['phone'].' dans les plus brefs délais pour traiter votre demande : </p>'.
 
-    '<p>" '.$_POST['message'].' "</p>';
+    if (in_array('', $_POST, true)){
+        echo 'Pour soumettre votre formulaire, merci de remplir les champs suivants : <br>';
+    foreach ($_POST as $key => $value ) {
+            if ($value === ''){
+               echo '- '.$key.'<br>';
+            }
+        }
+    } elseif ($_POST['email']){
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            echo 'Votre adresse email n\'est pas valide.';
+        } else {
+            echo '<h1>Merci '.$_POST['firstname'].' '.$_POST['lastname'].' de nous avoir contacté à propos de “'.$_POST['list'].'”.</h1>'.
+            '<p>  Un de nos conseiller vous contactera soit à l’adresse '.$_POST['email'].' ou par téléphone au '.$_POST['phone'].' dans les plus brefs délais pour traiter votre demande : </p>'.
+        
+            '<p>" '.$_POST['message'].' "</p>';
+    }
+   
+   }
+
+
+    
     ?>
     </div>
     
